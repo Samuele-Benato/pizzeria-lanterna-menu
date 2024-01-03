316b76ae26c957f870f4eab7d986251da755045e
Samuele-Benato <samuele.benato@gmail.com>
2024-01-03 15:04:25 +0100

    feat : add menu data

---
+++ src/data.js
@@ -0,0 +1,391 @@
+///////////////////
+//   ANTIPASTI  //
+//////////////////
+
+export const antipasti = [
+    {
+        name : 'Schiacciata',
+        ingredients: 'pasta di pizza, olio, origano, un velo di pomodoro',
+        price:'3,00 €',
+    },
+    {
+        name : 'Affettati misti',
+        ingredients: 'prosciutto crudo di Parma, spek trentino, salamino piccante, pancetta dolce',
+        price:'9,50 €',
+    },
+    {
+        name: 'Cipolle agrodolci',
+        ingredients: '',
+        price: '4,00 €',
+    },
+    {
+        name: 'Olive verdi o nere greche',
+        ingredients: '',
+        price: '4,00 €',
+    },
+    {
+        name: 'Insalata contadina',
+        ingredients: 'maionese, tonno, cipolline',
+        price: '4,50 €',
+    },
+    {
+        name: 'Insalata di pollo',
+        ingredients: 'maionese, pollo, verdure miste',
+        price: '4,50 €',
+    },
+    {
+        name: 'Insalata russa',
+        ingredients: 'maionese, verdure miste',
+        price: '4,00 €',
+    },  
+];
+
+/////////////////////////
+//   LE TRADIZIONALI  //
+////////////////////////
+
+export const tradizionali = [
+    {
+      name: 'BOSCAIOLA',
+      ingredients: 'pomodoro, mozzarella, champignon trifolati, porcini, chiodini, shiitake, volvariella, sbrise (funghi in proporzione variabile)',
+      price: '8 €'
+    },
+    {
+      name: 'BRESAOLA, RUCOLA E GRANA',
+      ingredients: 'pomodoro, mozzarella, bresaola della Valtellina IGP, rucola, grana',
+      price: '8,5 €'
+    },
+    {
+      name: 'BRIE SPECK',
+      ingredients: 'pomodoro, mozzarella, Brie, speck trentino',
+      price: '8 €'
+    },
+    {
+      name: 'CALZONE',
+      ingredients: 'mozzarella, prosciutto cotto, funghi champignon trifolati, carciofini a fine cottura, pomodoro',
+      price: '8 €'
+    },
+    {
+      name: 'CAPRICCIOSA',
+      ingredients: 'pomodoro, mozzarella, prosciutto cotto, champignon trifolati, carciofini',
+      price: '8 €'
+    },
+    {
+      name: 'CHIODINI',
+      ingredients: 'pomodoro, mozzarella, funghi chiodini',
+      price: '6,5 €'
+    },
+    {
+      name: 'DIAVOLA',
+      ingredients: 'pomodoro, mozzarella, salamino piccante',
+      price: '6 €'
+    },
+    {
+      name: 'FUNGHI',
+      ingredients: 'pomodoro, mozzarella, champignon trifolati',
+      price: '6,5 €'
+    },
+    {
+      name: 'GORGONZOLA',
+      ingredients: 'pomodoro, mozzarella, gorgonzola dolce DOP',
+      price: '6 €'
+    },
+    {
+      name: 'INGLESINA',
+      ingredients: 'pomodoro, mozzarella, prosciutto crudo di Parma',
+      price: '7,5 €'
+    },
+    {
+      name: 'MARGHERITA',
+      ingredients: 'pomodoro, mozzarella',
+      price: '4,50 €'
+    },
+    {
+      name: 'MARINARA',
+      ingredients: 'pomodoro, aglio, origano',
+      price: '4 €'
+    },
+    {
+      name: 'MELANZANE',
+      ingredients: 'pomodoro, mozzarella, melanzane ai ferri',
+      price: '6 €'
+    },
+    {
+      name: 'PEPERONI',
+      ingredients: 'pomodoro, mozzarella, peperoni',
+      price: '6 €'
+    },
+    {
+      name: 'PORCINI',
+      ingredients: 'pomodoro, mozzarella, funghi porcini',
+      price: '7 €'
+    },
+    {
+      name: 'PROSCIUTTO',
+      ingredients: 'pomodoro, mozzarella, prosciutto cotto',
+      price: '6 €'
+    },
+    {
+      name: 'PROSCIUTTO E FUNGHI',
+      ingredients: 'pomodoro, mozzarella, prosciutto cotto, champignon trifolati',
+      price: '7,5 €'
+    },
+    {
+      name: 'PROVINCIALE',
+      ingredients: 'pomodoro, mozzarella, acciughe, olive nere intere, origano',
+      price: '6,5 €'
+    },
+    {
+      name: 'PUGLIESE',
+      ingredients: 'pomodoro, mozzarella, cipolla di Tropea',
+      price: '6 €'
+    },
+    {
+      name: '4 FORMAGGI',
+      ingredients: 'pomodoro, mozzarella, crema di formaggi (No Gorgonzola)',
+      price: '6 €'
+    },
+    {
+      name: '4 STAGIONI',
+      ingredients: 'pomodoro, mozzarella, prosciutto cotto, champignon trifolati, carciofini (gusti separati)',
+      price: '8 €'
+    },
+    {
+      name: 'RADICCHIO',
+      ingredients: 'pomodoro, mozzarella, radicchio rosso spadone Treviso (solo stagionale)',
+      price: '6,5 €'
+    },
+    {
+      name: 'RICOTTA E SPINACI',
+      ingredients: 'pomodoro, mozzarella, ricotta, spinaci',
+      price: '6,5 €'
+    },
+    {
+      name: 'ROMANA',
+      ingredients: 'pomodoro, mozzarella, acciughe, origano',
+      price: '6 €'
+    },
+    {
+      name: 'TIROLESE',
+      ingredients: 'pomodoro, mozzarella, speck trentino',
+      price: '7,5 €'
+    },
+    {
+      name: 'TONNO E CIPOLLA',
+      ingredients: 'pomodoro, mozzarella, cipolla di Tropea, tonno',
+      price: '7,5 €'
+    },
+    {
+      name: 'UOVO E ASPARAGI',
+      ingredients: 'pomodoro, mozzarella, asparagi bianchi, uovo',
+      price: '6,5 €'
+    },
+    {
+      name: 'VIENNESE',
+      ingredients: 'pomodoro, mozzarella, wurstel',
+      price: '6 €'
+    },
+    {
+      name: 'VEGETARIANA',
+      ingredients: 'pomodoro, mozzarella, champignon trifolati, peperoni, carciofini, asparagi',
+      price: '8 €'
+    }
+  ];
+  
+/////////////////////
+//   LE ORIGINALI  //
+/////////////////////
+
+export const originali = [
+    {
+      name: '100 CITTÀ',
+      ingredients: 'pomodoro, mozzarella, prosciutto cotto, funghi champignon trifolati, carciofini, wurstel, salamino piccante, uovo',
+      price: '9,50 €'
+    },
+    {
+      name: 'SAPORITA',
+      ingredients: 'pomodoro, mozzarella, acciughe, cipolla di Tropea, salamino piccante, capperi, origano',
+      price: '8,00 €'
+    },
+    {
+      name: 'LANTERNA',
+      ingredients: 'pomodoro, mozzarella, funghi misti (vedi boscaiola), pancetta dolce, grana',
+      price: '9,50 €'
+    },
+    {
+      name: 'TREVIGIANA',
+      ingredients: 'pomodoro, mozzarella, radicchio rosso spadone Treviso, gorgonzola dolce DOP, pancetta dolce (solo stagionale)',
+      price: '9,00 €'
+    },
+    {
+      name: 'GUSTOSA',
+      ingredients: 'pomodoro, mozzarella, porcini, gorgonzola DOP, bresaola della Valtellina IGP, grana',
+      price: '9,50 €'
+    },
+    {
+      name: 'SPEZIATA',
+      ingredients: 'pomodoro, mozzarella, tonno, cipolla di Tropea, salamino piccante, gorgonzola dolce DOP, spezie',
+      price: '9,50 €'
+    },
+    {
+      name: 'FANTASIA',
+      ingredients: 'pomodoro, mozzarella, funghi champignon trifolati, salamino piccante, cipolla di Tropea, peperoni, gorgonzola dolce DOP, rucola, pancetta dolce, tonno (gusti separati composti a discrezione del pizzaiolo)',
+      price: '9,50 €'
+    }
+  ];
+  
+/////////////////////
+//   LE SPECIALI  //
+////////////////////
+
+export const speciali = [
+{
+    name: 'FAGOTTO LANTERNA',
+    ingredients: 'mozzarella, melanzane*, funghi champignon trifolati, gorgonzola dolce DOP a fine cottura: pomodoro, pancetta dolce, grana',
+    price: '10,00 €'
+},
+{
+    name: 'DISCO VOLANTE',
+    ingredients: 'mozzarella, prosciutto cotto, funghi champignon trifolati, carciofini a fine cottura: pomodoro',
+    price: '18,00 €'
+},
+{
+    name: 'PANINO',
+    ingredients: 'pane di pasta di pizza, olio al pomodoro, mozzarella, prosciutto crudo di Parma',
+    price: '7,50 €'
+},
+{
+    name: 'CROSTINO',
+    ingredients: 'pane bianco, pomodoro, mozzarella, prosciutto cotto',
+    price: '7,50 €'
+}
+];
+
+///////////////////////
+//   BIRRE IN BOTT  //
+//////////////////////
+
+export const bottleBeers = [
+{
+    name : 'Weihenstephaner Hefe',
+    grade: '(5,4°)',
+    description: 'Birra di frumento torbida. Dal gusto rotondo e rinfrescante con note di frutta matura e spezie',
+    price: '4,50 €'
+},
+{
+    name : 'Nastro Azzurro Analcolica',
+    grade: '(0,5° 33cl)',
+    description: '',
+    price: '3,50 €'
+},
+{
+    name : 'Beck’s',
+    grade: '(5° 33cl)',
+    description: '',
+    price: '4,00 €'
+},
+]; 
+
+/////////////////////
+//   BIBITE SPINA //
+////////////////////
+
+export const draftDrinks = [
+{
+    name: 'Nastro Azzurro (20cl)',
+    grade: '(5,2°)',
+    description: 'Birra di mais dal gusto semplice e rinfrescante, con finale leggermente amarognolo',
+    price: '3,00 €'
+},
+{
+    name: 'Nastro Azzurro (40cl)',
+    grade: '(5,2°)',
+    description: '',
+    price: '4,00 €'
+},
+{
+    name: 'Ceres Red Erik (20cl)',
+    grade: '(6,5°)',
+    description: 'Birra rossa dal gusto morbido e dolce, che ricorda la frutta fresca il malto e il caramello.',
+    price: '3,50 €'
+},
+{
+    name: 'Ceres Red Erik (40cl)',
+    grade: '(6,5°)',
+    description: '',
+    price: '4,50 €'
+},
+{
+    name: 'Ceres Strong Ale (20cl)',
+    grade: '(7,7°)',
+    description: 'Birra bionda dal carattere forte, dal gusto pieno e intenso.',
+    price: '3,50 €'
+},
+{
+    name: 'Ceres Strong Ale (40cl)',
+    grade: '(7,7°)',
+    description: '',
+    price: '4,50 €'
+},
+{
+    name: 'Coca Cola (30cl)',
+    grade: '',
+    description: '',
+    price: '3,00 €'
+},
+{
+    name: 'Coca Cola (40cl)',
+    grade: '',
+    description: '',
+    price: '3,50 €'
+},
+{
+    name: 'Tè limone o pesca (33cl)',
+    grade: '',
+    description: '',
+    price: '3,50 €'
+},
+]
+
+///////////////
+//  DESSERT //
+//////////////
+
+export const dessert = [
+    {
+        name : 'Torta di mandorle',
+        price: '3,50 €',
+    },
+    {
+        name : 'Crostata di albicocca',
+        price: '3,50 €',
+    },
+    {
+        name : 'Tartufo nero',
+        price: '3,50 €',
+    },
+    {
+        name : 'Tartufo bianco',
+        price: '3,50 €',
+    },
+    {
+        name : 'Profiteroles al cioccolato',
+        price: '3,50 €',
+    },
+    {
+        name : 'Profiteroles al tiramisù',
+        price: '3,50 €',
+    },
+    {
+        name : 'Meringata',
+        price: '4,00 €',
+    },
+    {
+        name : 'Biscotti artiginali al cioccolato',
+        price: '1,00 €',
+    },
+    {
+        name : 'Biscotti artiginali alla marmellata',
+        price: '1,00 €',
+    },
+]
---
+++ src/main.js
@@ -0,0 +1,27 @@
+import { createApp } from 'vue'
+// import './style.css'
+import App from './App.vue'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as bootstrap from "bootstrap";
+
+/* Set up using Vue 3 */
+import { createApp } from 'vue'
+import App from './App.vue'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
+
